a35dabf63453d107e60685803058d3fa72cc7d6c
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-06-27 15:20:28 -0400

    Move server into a function

--- server.py
+++ server.py
@@ -3,22 +3,28 @@ import zmq
 
 RESTART = 'max_id.txt'
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
 
-if not os.path.exists(RESTART):
-    with open(RESTART, 'w') as fout:
-        fout.write('0')
+def serve(port=5555):
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind('tcp://*:%s' % port)
 
-with open(RESTART) as fin:
-    curr_id = int(fin.read())
+    if not os.path.exists(RESTART):
+        with open(RESTART, 'w') as fout:
+            fout.write('0')
 
-while True:
-    ids_req = socket.recv_json()
-    ids_rep = {'begin': curr_id}
-    curr_id += ids_req['count']
-    ids_rep['end'] = curr_id
-    with open(RESTART, 'w') as fout:
-        fout.write(str(curr_id))
-    socket.send_json(ids_rep)
+    with open(RESTART) as fin:
+        curr_id = int(fin.read())
+
+    while True:
+        ids_req = socket.recv_json()
+        ids_rep = {'begin': curr_id}
+        curr_id += ids_req['count']
+        ids_rep['end'] = curr_id
+        with open(RESTART, 'w') as fout:
+            fout.write(str(curr_id))
+        socket.send_json(ids_rep)
+
+
+if __name__ == '__main__':
+    serve()
