db1719ff23eba92bbcb9e5ace44f1e98d4bbf689
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-06-27 15:07:56 -0400

    Use JSON instead of ProtoBuf for now

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import zmq
 import sys
 import argparse
-import ids_pb2
 
 parser = argparse.ArgumentParser()
 parser.add_argument("count", type=int, help="wanted IDs count")
@@ -14,12 +13,9 @@ context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
-ids_req = ids_pb2.IdsRequest()
-ids_req.count = args.count
+ids_req = {'count': args.count}
 print("Requesting %d ids.." % ids_req.count)
-socket.send(ids_req.SerializeToString())
+socket.send_json(ids_req)
 
-msg = socket.recv()
-ids_rep = ids_pb2.IdsReply()
-ids_rep.ParseFromString(msg)
-print("My ids are %s" % range(ids_rep.begin, ids_rep.end))
+ids_rep = socket.recv_json()
+print("My ids are %s to %s" % (ids_rep['begin'], ids_rep['end']))
--- server.py
+++ server.py
@@ -1,20 +1,13 @@
 import zmq
-import ids_pb2
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
 curr_id, max_id = 0, 2**64-1
-max_count = 10000
 while True:
-    msg = socket.recv()
-    ids_req = ids_pb2.IdsRequest()
-    ids_req.ParseFromString(msg)
-
-    ids_rep = ids_pb2.IdsReply()
-    ids_rep.begin = curr_id
-    curr_id = min(max_id, curr_id + min(ids_req.count, max_count))
-    ids_rep.end = curr_id
-
-    socket.send(ids_rep.SerializeToString())
+    ids_req = socket.recv_json()
+    ids_rep = {'begin': curr_id}
+    curr_id += ids_req['count']
+    ids_rep['end'] = curr_id
+    socket.send_json(ids_rep)
