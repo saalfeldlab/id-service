bf5dabad5cbd1292a45202591ee13203d8d769f6
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-06-27 15:26:33 -0400

    Move client interface into a function

--- client.py
+++ client.py
@@ -1,21 +1,24 @@
 import zmq
-import sys
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("count", type=int, help="wanted IDs count")
-args = parser.parse_args()
-if args.count < 0:
-    print("the count can't be negative")
-    sys.exit()
 
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
 
-ids_req = {'count': args.count}
-print("Requesting %d ids.." % ids_req.count)
-socket.send_json(ids_req)
+def request(count):
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
 
-ids_rep = socket.recv_json()
-print("My ids are %s to %s" % (ids_rep['begin'], ids_rep['end']))
+    ids_req = {'count': args.count}
+    print("Requesting %d ids.." % ids_req.count)
+    socket.send_json(ids_req)
+
+    ids_rep = socket.recv_json()
+    return ids_rep['begin'], ids_rep['end']
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    start, end = request(args.count)
+    print('got ids %s to %s.' % (start, end))
