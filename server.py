6acce620e81b3bdac5691b05aa7932abadedb5ab
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-06-27 15:16:36 -0400

    Use simple text file to ensure smooth restarts

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
+import os
 import zmq
+
+RESTART = 'max_id.txt'
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
-curr_id, max_id = 0, 2**64-1
+if not os.path.exists(RESTART):
+    with open(RESTART, 'w') as fout:
+        fout.write('0')
+
+with open(RESTART) as fin:
+    curr_id = int(fin.read())
+
 while True:
     ids_req = socket.recv_json()
     ids_rep = {'begin': curr_id}
     curr_id += ids_req['count']
     ids_rep['end'] = curr_id
+    with open(RESTART, 'w') as fout:
+        fout.write(str(curr_id))
     socket.send_json(ids_rep)
